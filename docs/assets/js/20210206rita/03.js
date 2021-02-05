bde2161d4e6ef6417f01b83c4ceca2abfa80efb4
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2021-02-05 21:22:17 +0000

    Create 03.js

---
+++ docs/assets/js/20210206rita/03.js
@@ -0,0 +1,18 @@
+function checkPassword() {
+  if (document.getElementById("password").value == "lisbon") {
+    alert("Correct Password!");
+    location.href = "https://www.albertsmysteries.com/20210206rita/04/";
+  } else {
+    alert("Wrong Password!");
+    return false;
+  }
+}
+
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}

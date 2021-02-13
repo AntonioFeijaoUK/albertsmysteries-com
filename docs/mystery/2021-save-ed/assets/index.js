aca4e692e2e47b70425cb65e332b5e0ea2693965
Antonio Feijao UK <2548084+AntonioFeijaoUK@users.noreply.github.com>
2021-02-13 21:46:32 +0000

    Update index.js

--- docs/mystery/2021-save-ed/assets/index.js
+++ docs/mystery/2021-save-ed/assets/index.js
@@ -3,3 +3,38 @@ console.log("Hello world!");
 function myFunction() {
   document.getElementById("demo").innerHTML = "CLICAS-TE no botao! page index.js";
 }
+
+
+
+
+function clickSIM() {
+  alert("FIXE! Muito obrigado, vamos a isso!");
+    location.href = "./01/";
+  } else {
+    alert("Wrong!");
+    return false;
+  }
+}
+
+
+
+
+function clickNAO() {
+  alert("Ohhh minha nossa....");
+    location.href = "./";
+  } else {
+    alert("Wrong!");
+    return false;
+  }
+}
+
+
+function checkPassword() {
+  if (document.getElementById("password").value == "pudding lane") {
+    alert("Correct!");
+    location.href = "https://www.albertsmysteries.com/20210206rita/02/";
+  } else {
+    alert("Wrong!");
+    return false;
+  }
+}
